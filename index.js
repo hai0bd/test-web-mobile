c10fe1076cee89aea036c130e2d9e22b35c786ce
hai0bd <dosonhai2004@gmail.com>
2024-10-12 10:17:58 +0700

    update version 1.0.1

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-System.register(["./application.9b109.js"], function (_export, _context) {
+System.register(["./application.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
@@ -6,8 +6,8 @@ System.register(["./application.9b109.js"], function (_export, _context) {
     return System["import"](url);
   }
   return {
-    setters: [function (_application9b109Js) {
-      Application = _application9b109Js.Application;
+    setters: [function (_applicationJs) {
+      Application = _applicationJs.Application;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
